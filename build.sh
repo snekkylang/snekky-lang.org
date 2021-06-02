dabe6ee6605c84a58a6b8503046d006ac38f6f5f
SenkJu <18741573+SenkJu@users.noreply.github.com>
2021-06-02 17:47:37 +0200

    add workflow

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Building snekky..."
+cd snekky
+haxelib install console.hx 0.2.12 --quiet
+haxelib install hxcpp --quiet
+haxelib install hxargs --quiet
+haxe build-js.hxml
+cp js/snekky.js ../public/js/
+cd ..
+
+echo "Building snekkyd..."
+cd snekkyd
+haxelib install hxcpp --quiet
+haxe build-js.hxml
+cp js/snekkyd.js ../public/js/
+cd ..
+
+echo "Building snekkyp.."
+cd snekkyd
+haxelib install hxcpp --quiet
+haxe build-js.hxml
+cp js/snekkyp.js ../public/js/
+cd ..
+
+echo "Building website..."
+npm install
+npm run build
